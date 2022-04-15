30aa54c0041deffdb27ca3a720aed739275ee0b7
David Blagojevic <e11904645@student.tuwien.ac.at>
2022-04-15 03:39:07 +0200

    Some pointers for muxi and commented code in the backend

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const app = express();
 const expressWs = require('express-ws')(app);
 const port = 8080;
 
-let turtles = [{id: 1, name: "a"}, {id: 2, name: "b"}]
-let altTurtles = [{id: 2, name: "b"}, {id: 3, name:"c"}, {id: 4, name:"d"}]
-let alotOfTurtles = [{id: 1, name: "a"},{id: 2, name: "b"},{id: 3, name:"c"},{id: 4, name:"d"},{id: 5, name:"e"},{id: 6, name:"f"},{id: 7, name:"g"},{id: 8, name:"h"},{id: 9, name:"i"},{id: 10, name:"j"},{id: 11, name:"k"},{id: 12, name:"l"},{id: 13, name:"m"},{id: 14, name:"n"},{id: 15, name:"o"},{id: 16, name:"p"},{id: 17, name:"q"},{id: 18, name:"r"}
+const turtles = [{id: 1, name: "a"},{id: 2, name: "b"},{id: 3, name:"c"},{id: 4, name:"d"},{id: 5, name:"e"},{id: 6, name:"f"},{id: 7, name:"g"},{id: 8, name:"h"},{id: 9, name:"i"},{id: 10, name:"j"},{id: 11, name:"k"},{id: 12, name:"l"},{id: 13, name:"m"},{id: 14, name:"n"},{id: 15, name:"o"},{id: 16, name:"p"},{id: 17, name:"q"},{id: 18, name:"r"}
 ]
 
 app.ws("/", (ws, req) => {
@@ -18,8 +16,6 @@ app.ws("/", (ws, req) => {
         console.log(parsedMessage);
         if (parsedMessage.type === "get") {
             ws.send(JSON.stringify({type: "full", turtles: turtles}))
-            ws.send(JSON.stringify({type: "f", turtle: {id: 3, name: "c"}}))
-            ws.send(JSON.stringify({type: "full", turtles: alotOfTurtles}))
             //ws.send(JSON.stringify({type: "bye", turtle: {id: 3, name:"c"}}))
         }
     } catch {}
@@ -32,7 +28,32 @@ app.ws("/", (ws, req) => {
   
 });
 
+// app.ws("/turtle", (ws, req) => {
+//   console.log("Irgendeine Turtle ist jetzt a dabei");
+//   turtle = ws;
+
+//   ws.on("message", msg => {
+//     const parsedMessage = JSON.parse(msg);
+//     console.log(parsedMessage);
+//     if (typeof turtle !== 'undefined') {
+//       user.send(msg);
+//     }  
+//   });
+
+//   ws.on("close", e => {
+//     console.log("Irgendeine Turtle ist jetzt nima dabei");
+//     turtle = undefined;
+//   });
+  
+// });
+
 app.listen(port, () => {
   console.log("Server listening on port:", port);
 });
 
+/*
+Handle connections with turtles
+Handle connection with users
+Status message to users (new turtle, turtle gone or block update with subscription)
+database
+*/
